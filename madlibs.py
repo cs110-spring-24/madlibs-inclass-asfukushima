c623ba4b049617d7f4f0a948e64c63c0bd03c933
asfukushima <arisafukushima12@gmail.com>
2024-01-30 13:08:01 -0800

    Update madlibs.py

--- madlibs.py
+++ madlibs.py
@@ -1,4 +1,5 @@
 # TODO Create your inputs, variables, and add your story here!noun = input("Enter a noun: ")
+noun - input("Enter a noun: ")
 adjective = input("Enter an adjective: ")
 verb = input ("Enter a verb: ") 
 print(f"The", {noun}, "was extremely", {adjective}, "and", {verb}, "with their friends")
